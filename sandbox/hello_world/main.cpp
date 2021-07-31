f1619df0ef146875f87a25311b4dd86ec4ffd83d
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-07-31 14:28:33 -0400

    Trying out new features

--- sandbox/hello_world/main.cpp
+++ sandbox/hello_world/main.cpp
@@ -1,23 +1,30 @@
 
 #include <engine/graphics/window.h>
+#include <iostream>
 
 int main(int, char**)
 {
   using namespace genebits::engine;
 
-  constexpr WindowCreationHints hints = WindowCreationHints::Maximised | WindowCreationHints::Resizable | WindowCreationHints::FocusingOnShow | WindowCreationHints::AutoIconified | WindowCreationHints::Decorated;
+  constexpr WindowCreationHints hints = WindowCreationHints::Defaults;
   auto* window = new Window("Hello world", 256, 256, hints);
 
   window->Create();
 
   window->SetWindowClosingCallback([](Window* window_ptr)
-    { window_ptr->Close(); });
+    {
+      window_ptr->Close();
+      std::cout << "closing" << std::endl;
+    });
 
   unsigned long long i = 0;
-  while (i < (UINT32_MAX >> 1) - 1)
+  while (i < 300)
   {
+    window->WaitEvents(0.5);
     i++;
   }
+
+  window->Close();
 
   return 0;
 }
