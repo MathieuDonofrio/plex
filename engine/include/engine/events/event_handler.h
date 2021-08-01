a9e661a5b99285dc53e842d27eadc69cb3f6b259
LAPTOP-EK3CB67C\Mathieu <ptimatt@live.ca>
2021-08-01 16:43:42 -0400

    event handler improvements

--- engine/include/engine/events/event_handler.h
+++ engine/include/engine/events/event_handler.h
@@ -73,27 +73,45 @@ namespace
     FunctionType function_;
     ClassType* instance_;
   };
+
+  template<typename Event>
+  constexpr size_t LargestFunctorSize()
+  {
+    class FakeClass
+    {};
+
+    size_t max = 0;
+
+    max = sizeof(FreeEventFunctor<Event>) > max ? sizeof(FreeEventFunctor<Event>) : max;
+    max = sizeof(MemberEventFunctor<FakeClass, Event>) > max ? sizeof(MemberEventFunctor<FakeClass, Event>) : max;
+    max = sizeof(ConstMemberEventFunctor<FakeClass, Event>) > max ? sizeof(ConstMemberEventFunctor<FakeClass, Event>) : max;
+
+    return max;
+  }
 } // namespace
 
 template<typename Event>
 class EventHandler
 {
 public:
-  static constexpr size_t cLargestFunctorSize = sizeof(&EventFunctor<Event>::operator()) + sizeof(void*);
+  static constexpr size_t cLargestFunctorSize = LargestFunctorSize<Event>();
 
   constexpr EventHandler(void (*function)(const Event& event)) noexcept
+    : functor_ {}
   {
     new (functor_) FreeEventFunctor<Event>(function);
   }
 
   template<typename ClassType>
   constexpr EventHandler(void (ClassType::*function)(const Event& event), ClassType* instance) noexcept
+    : functor_ {}
   {
     new (functor_) MemberEventFunctor<ClassType, Event>(function, instance);
   }
 
   template<typename ClassType>
   constexpr EventHandler(void (ClassType::*function)(const Event& event) const, ClassType* instance) noexcept
+    : functor_ {}
   {
     new (functor_) ConstMemberEventFunctor<ClassType, Event>(function, instance);
   }
@@ -101,6 +119,21 @@ public:
   void Invoke(const Event& event)
   {
     (*reinterpret_cast<EventFunctor<Event>*>(&functor_))(event);
+  }
+
+  [[nodiscard]] bool operator==(EventHandler<Event> handler) const noexcept
+  {
+    for (size_t i = 0; i < cLargestFunctorSize; i++)
+    {
+      if (functor_[i] != handler.functor_[i]) return false;
+    }
+
+    return true;
+  }
+
+  [[nodiscard]] bool operator!=(EventHandler<Event> handler) const noexcept
+  {
+    return !(*this == handler);
   }
 
 private:
--- engine/test/events/event_handler_tests.cpp
+++ engine/test/events/event_handler_tests.cpp
@@ -1,5 +1,7 @@
 #include "engine/events/event_handler.h"
 
+#include <type_traits>
+
 #include <gtest/gtest.h>
 
 namespace genebits::engine::tests
@@ -8,33 +10,51 @@ namespace
 {
   struct TestEvent
   {
-    size_t value1;
-    double value2;
+    volatile size_t value;
   };
 
   struct TestListener
   {
     void addValue1(const TestEvent& event)
     {
-      sumValue1 += event.value1;
+      sumValue1 += event.value;
     }
 
-    void addValueConst1(const TestEvent& event)
+    void addValue2(const TestEvent& event)
     {
-      sumValue1 += event.value1;
+      sumValue2 += event.value;
+    }
+
+    void addValueConst1(const TestEvent& event) const
+    {
+      sumValue1 += event.value;
+    }
+
+    void addValueConst2(const TestEvent& event) const
+    {
+      sumValue2 += event.value;
     }
 
     mutable size_t sumValue1 = 0;
+    mutable size_t sumValue2 = 0;
   };
 
   size_t sumValue1Global = 0u;
+  size_t sumValue2Global = 0u;
 
   void addValue1Global(const TestEvent& event)
   {
-    sumValue1Global += event.value1;
+    sumValue1Global += event.value;
+  }
+
+  void addValue2Global(const TestEvent& event)
+  {
+    sumValue2Global += event.value;
   }
 
 } // namespace
+
+static_assert(std::is_trivially_copyable_v<EventHandler<TestEvent>>, "EventHandler must be trivially copyable");
 
 TEST(EventHandler_Tests, Invoke_FreeFunction_DelegatesCall)
 {
@@ -42,11 +62,11 @@ TEST(EventHandler_Tests, Invoke_FreeFunction_DelegatesCall)
 
   EventHandler<TestEvent> handler(addValue1Global);
 
-  handler.Invoke({ 1u, 0 });
+  handler.Invoke({ 1u });
 
   ASSERT_EQ(sumValue1Global, 1u);
 
-  handler.Invoke({ 10u, 0 });
+  handler.Invoke({ 10u });
 
   ASSERT_EQ(sumValue1Global, 11u);
 }
@@ -57,11 +77,11 @@ TEST(EventHandler_Tests, Invoke_MemberFunction_DelegatesCall)
 
   EventHandler<TestEvent> handler(&TestListener::addValue1, &listener);
 
-  handler.Invoke({ 1u, 0 });
+  handler.Invoke({ 1u });
 
   ASSERT_EQ(listener.sumValue1, 1u);
 
-  handler.Invoke({ 10u, 0 });
+  handler.Invoke({ 10u });
 
   ASSERT_EQ(listener.sumValue1, 11u);
 }
@@ -72,12 +92,137 @@ TEST(EventHandler_Tests, Invoke_ConstMemberFunction_DelegatesCall)
 
   EventHandler<TestEvent> handler(&TestListener::addValueConst1, &listener);
 
-  handler.Invoke({ 1u, 0 });
+  handler.Invoke({ 1u });
 
   ASSERT_EQ(listener.sumValue1, 1u);
 
-  handler.Invoke({ 10u, 0 });
+  handler.Invoke({ 10u });
 
   ASSERT_EQ(listener.sumValue1, 11u);
+}
+
+TEST(EventHandler_Tests, Equality_FreeFunction_Equal)
+{
+  EventHandler<TestEvent> handler(&addValue1Global);
+
+  ASSERT_EQ(handler, handler);
+}
+
+TEST(EventHandler_Tests, Equality_FreeFunction_NotEqual)
+{
+  EventHandler<TestEvent> handler1(&addValue1Global);
+  EventHandler<TestEvent> handler2(&addValue2Global);
+
+  ASSERT_NE(handler1, handler2);
+}
+
+TEST(EventHandler_Tests, Equality_MemberFunction_Equal)
+{
+  TestListener listener;
+
+  EventHandler<TestEvent> handler(&TestListener::addValue1, &listener);
+
+  ASSERT_EQ(handler, handler);
+}
+
+TEST(EventHandler_Tests, Equality_MemberFunction_FunctionNotEqual)
+{
+  TestListener listener;
+
+  EventHandler<TestEvent> handler1(&TestListener::addValue1, &listener);
+  EventHandler<TestEvent> handler2(&TestListener::addValue2, &listener);
+
+  ASSERT_NE(handler1, handler2);
+}
+
+TEST(EventHandler_Tests, Equality_MemberFunction_InstanceNotEqual)
+{
+  TestListener listener1;
+  TestListener listener2;
+
+  EventHandler<TestEvent> handler1(&TestListener::addValue1, &listener1);
+  EventHandler<TestEvent> handler2(&TestListener::addValue1, &listener2);
+
+  ASSERT_NE(handler1, handler2);
+}
+
+TEST(EventHandler_Tests, Equality_MemberFunction_NotEqual)
+{
+  TestListener listener1;
+  TestListener listener2;
+
+  EventHandler<TestEvent> handler1(&TestListener::addValue1, &listener1);
+  EventHandler<TestEvent> handler2(&TestListener::addValue2, &listener2);
+
+  ASSERT_NE(handler1, handler2);
+}
+
+TEST(EventHandler_Tests, Equality_ConstMemberFunction_Equal)
+{
+  TestListener listener;
+
+  EventHandler<TestEvent> handler(&TestListener::addValueConst1, &listener);
+
+  ASSERT_EQ(handler, handler);
+}
+
+TEST(EventHandler_Tests, Equality_ConstMemberFunction_FunctionNotEqual)
+{
+  TestListener listener;
+
+  EventHandler<TestEvent> handler1(&TestListener::addValueConst1, &listener);
+  EventHandler<TestEvent> handler2(&TestListener::addValueConst2, &listener);
+
+  ASSERT_NE(handler1, handler2);
+}
+
+TEST(EventHandler_Tests, Equality_ConstMemberFunction_InstanceNotEqual)
+{
+  TestListener listener1;
+  TestListener listener2;
+
+  EventHandler<TestEvent> handler1(&TestListener::addValueConst1, &listener1);
+  EventHandler<TestEvent> handler2(&TestListener::addValueConst1, &listener2);
+
+  ASSERT_NE(handler1, handler2);
+}
+
+TEST(EventHandler_Tests, Equality_ConstMemberFunction_NotEqual)
+{
+  TestListener listener1;
+  TestListener listener2;
+
+  EventHandler<TestEvent> handler1(&TestListener::addValueConst2, &listener1);
+  EventHandler<TestEvent> handler2(&TestListener::addValueConst2, &listener2);
+
+  ASSERT_NE(handler1, handler2);
+}
+
+TEST(EventHandler_Tests, CopyAssignment_FreeFunction_Equal)
+{
+  EventHandler<TestEvent> handler1(addValue1Global);
+  EventHandler<TestEvent> handler2 = handler1;
+
+  ASSERT_EQ(handler1, handler2);
+}
+
+TEST(EventHandler_Tests, CopyAssignment_MemberFunction_Equal)
+{
+  TestListener listener;
+
+  EventHandler<TestEvent> handler1(&TestListener::addValue1, &listener);
+  EventHandler<TestEvent> handler2 = handler1;
+
+  ASSERT_EQ(handler1, handler2);
+}
+
+TEST(EventHandler_Tests, CopyAssignment_ConstMemberFunction_Equal)
+{
+  TestListener listener;
+
+  EventHandler<TestEvent> handler1(&TestListener::addValueConst1, &listener);
+  EventHandler<TestEvent> handler2 = handler1;
+
+  ASSERT_EQ(handler1, handler2);
 }
 } // namespace genebits::engine::tests
