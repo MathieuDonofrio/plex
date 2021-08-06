52067e99789da690a727d94473422862959c916f
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-08-06 17:56:58 -0400

    Added bitwise assigment operators

--- engine/include/engine/util/enum_flag.h
+++ engine/include/engine/util/enum_flag.h
@@ -17,37 +17,58 @@ struct EnableBitwiseOperators;
   enum class Name : Type
 
 template<class E>
-concept EnumFlags = std::is_enum_v<E>&& requires
+concept EnumFlags = std::is_enum_v<E> && requires
 {
   EnableBitwiseOperators<E> {};
 };
 
 template<EnumFlags EnumType>
-constexpr EnumType operator|(EnumType lhs, EnumType rhs) noexcept
+constexpr EnumType operator|(const EnumType lhs, const EnumType rhs) noexcept
 {
   using underlying = std::underlying_type_t<EnumType>;
   return static_cast<EnumType>(static_cast<underlying>(lhs) | static_cast<underlying>(rhs));
 }
 
 template<EnumFlags EnumType>
-constexpr EnumType operator&(EnumType lhs, EnumType rhs) noexcept
+constexpr EnumType operator&(const EnumType lhs, const EnumType rhs) noexcept
 {
   using underlying = std::underlying_type_t<EnumType>;
   return static_cast<EnumType>(static_cast<underlying>(lhs) & static_cast<underlying>(rhs));
 }
 
 template<EnumFlags EnumType>
-constexpr bool operator!=(EnumType lhs, std::underlying_type_t<EnumType> rhs) noexcept
+constexpr EnumType operator~(const EnumType rhs) noexcept
+{
+  using underlying = std::underlying_type_t<EnumType>;
+  return static_cast<EnumType>(~static_cast<underlying>(rhs));
+}
+
+template<EnumFlags EnumType>
+constexpr bool operator!=(const EnumType lhs, const std::underlying_type_t<EnumType> rhs) noexcept
 {
   using underlying = std::underlying_type_t<EnumType>;
   return static_cast<underlying>(lhs) != rhs;
 }
 
 template<EnumFlags EnumType>
-constexpr bool operator==(EnumType lhs, std::underlying_type_t<EnumType> rhs) noexcept
+constexpr bool operator==(const EnumType lhs, const std::underlying_type_t<EnumType> rhs) noexcept
 {
   using underlying = std::underlying_type_t<EnumType>;
   return static_cast<underlying>(lhs) == rhs;
+}
+
+template<EnumFlags EnumType>
+constexpr EnumType& operator&=(EnumType& rhs, const EnumType lhs) noexcept
+{
+  rhs = rhs & lhs;
+  return rhs;
+}
+
+template<EnumFlags EnumType>
+constexpr EnumType& operator|=(EnumType& rhs, const EnumType lhs) noexcept
+{
+  rhs = rhs | lhs;
+  return rhs;
 }
 
 } // namespace genebits::engine
--- engine/test/util/enum_flag_tests.cpp
+++ engine/test/util/enum_flag_tests.cpp
@@ -42,13 +42,37 @@ concept EnumBitwiseOr = requires(T enum_flag)
   };
 };
 
+template<typename T>
+concept EnumBitwiseComplement = requires(T enum_flag)
+{
+  {
+    ~enum_flag
+  };
+};
+
+template<typename T>
+concept EnumBitwiseAndAssignment = requires(T enum_flag, T enum_flag2)
+{
+  {
+    enum_flag &= enum_flag2
+  };
+};
+
+template<typename T>
+concept EnumBitwiseOrAssignment = requires(T enum_flag, T enum_flag2)
+{
+  {
+    enum_flag |= enum_flag2
+  };
+};
+
 template<typename E, typename T>
 concept EnumLogicNotEqual = requires(E enum_flag, T compared_type)
 {
   {
     enum_flag != compared_type
-  }
-  ->std::same_as<bool>;
+    }
+    -> std::same_as<bool>;
 };
 
 template<typename E, typename T>
@@ -56,14 +80,17 @@ concept EnumLogicEqual = requires(E enum_flag, T compared_type)
 {
   {
     enum_flag == compared_type
-  }
-  ->std::same_as<bool>;
+    }
+    -> std::same_as<bool>;
 };
 
-TEST(Enum_Flag_Tests, OperatorOverload_Specialized)
+TEST(Enum_Flag_Tests, OperatorOverload_Specialized_Compilable)
 {
   ASSERT_TRUE(EnumBitwiseAnd<MockEnumSpecialized>);
   ASSERT_TRUE(EnumBitwiseOr<MockEnumSpecialized>);
+  ASSERT_TRUE(EnumBitwiseComplement<MockEnumSpecialized>);
+  ASSERT_TRUE(EnumBitwiseOrAssignment<MockEnumSpecialized>);
+  ASSERT_TRUE(EnumBitwiseAndAssignment<MockEnumSpecialized>);
   ASSERT_TRUE((EnumLogicNotEqual<MockEnumSpecialized, uint64_t>));
   ASSERT_TRUE((EnumLogicEqual<MockEnumSpecialized, uint64_t>));
 }
@@ -72,6 +99,9 @@ TEST(Enum_Flag_Tests, OperatorOverload_NotSpecialized)
 {
   ASSERT_FALSE(EnumBitwiseAnd<MockEnumNotSpecialized>);
   ASSERT_FALSE(EnumBitwiseOr<MockEnumNotSpecialized>);
+  ASSERT_FALSE(EnumBitwiseComplement<MockEnumNotSpecialized>);
+  ASSERT_FALSE(EnumBitwiseOrAssignment<MockEnumNotSpecialized>);
+  ASSERT_FALSE(EnumBitwiseAndAssignment<MockEnumNotSpecialized>);
   ASSERT_FALSE((EnumLogicNotEqual<MockEnumNotSpecialized, uint64_t>));
   ASSERT_FALSE((EnumLogicEqual<MockEnumNotSpecialized, uint64_t>));
 }
@@ -80,6 +110,9 @@ TEST(Enum_Flag_Tests, OperatorOverload_WrongType)
 {
   ASSERT_FALSE(EnumBitwiseAnd<NotAnEnum>);
   ASSERT_FALSE(EnumBitwiseOr<NotAnEnum>);
+  ASSERT_FALSE(EnumBitwiseComplement<NotAnEnum>);
+  ASSERT_FALSE(EnumBitwiseOrAssignment<NotAnEnum>);
+  ASSERT_FALSE(EnumBitwiseAndAssignment<NotAnEnum>);
   ASSERT_FALSE((EnumLogicNotEqual<NotAnEnum, uint64_t>));
   ASSERT_FALSE((EnumLogicEqual<NotAnEnum, uint64_t>));
 }
