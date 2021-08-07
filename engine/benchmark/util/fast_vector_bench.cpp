e631618593394a03e24c79851dd39f986c82ff1f
LAPTOP-EK3CB67C\Mathieu <ptimatt@live.ca>
2021-08-07 19:39:55 -0400

    FastVector Optimizations, Tests & benchmarks

---
+++ engine/benchmark/util/fast_vector_bench.cpp
@@ -0,0 +1,245 @@
+#include "engine/util/fast_vector.h"
+
+#include <benchmark/benchmark.h>
+
+namespace genebits::engine
+{
+static void FastVector_STD_Vector_Iterate(benchmark::State& state)
+{
+  constexpr size_t cAmount = 10;
+
+  std::vector<size_t> vector;
+
+  for (size_t i = 0; i != cAmount; i++)
+  {
+    vector.push_back(i);
+  }
+
+  for (auto _ : state)
+  {
+    for (auto value : vector)
+    {
+      benchmark::DoNotOptimize(value);
+    }
+  }
+
+  benchmark::DoNotOptimize(vector);
+}
+
+BENCHMARK(FastVector_STD_Vector_Iterate);
+
+static void FastVector_STD_Vector_NonTrivial_Iterate(benchmark::State& state)
+{
+  constexpr size_t cAmount = 10;
+
+  std::vector<std::string> vector;
+
+  for (size_t i = 0; i != cAmount; i++)
+  {
+    vector.push_back(std::to_string(i));
+  }
+
+  for (auto _ : state)
+  {
+    for (auto& value : vector)
+    {
+      benchmark::DoNotOptimize(value);
+    }
+  }
+
+  benchmark::DoNotOptimize(vector);
+}
+
+BENCHMARK(FastVector_STD_Vector_NonTrivial_Iterate);
+
+static void FastVector_STD_Vector_PushBack(benchmark::State& state)
+{
+  for (auto _ : state)
+  {
+    std::vector<size_t> vector;
+
+    vector.push_back(10);
+
+    benchmark::DoNotOptimize(vector);
+  }
+}
+
+BENCHMARK(FastVector_STD_Vector_PushBack);
+
+static void FastVector_STD_Vector_PushBack_10(benchmark::State& state)
+{
+  constexpr size_t cAmount = 10;
+
+  for (auto _ : state)
+  {
+    std::vector<size_t> vector;
+
+    for (size_t i = 0; i != cAmount; i++)
+    {
+      vector.push_back(i);
+    }
+
+    benchmark::DoNotOptimize(vector);
+  }
+}
+
+BENCHMARK(FastVector_STD_Vector_PushBack_10);
+
+static void FastVector_STD_Vector_PushBack_100(benchmark::State& state)
+{
+  constexpr size_t cAmount = 100;
+
+  for (auto _ : state)
+  {
+    std::vector<size_t> vector;
+
+    for (size_t i = 0; i != cAmount; i++)
+    {
+      vector.push_back(i);
+    }
+
+    benchmark::DoNotOptimize(vector);
+  }
+}
+
+BENCHMARK(FastVector_STD_Vector_PushBack_100);
+
+static void FastVector_STD_Vector_PushBack_NonTrivial_100(benchmark::State& state)
+{
+  constexpr size_t cAmount = 100;
+
+  for (auto _ : state)
+  {
+    std::vector<std::string> vector;
+
+    for (size_t i = 0; i != cAmount; i++)
+    {
+      vector.push_back("TestingString123123");
+    }
+
+    benchmark::DoNotOptimize(vector);
+  }
+}
+
+BENCHMARK(FastVector_STD_Vector_PushBack_NonTrivial_100);
+
+static void FastVector_Iterate(benchmark::State& state)
+{
+  constexpr size_t cAmount = 10;
+
+  FastVector<size_t> vector;
+
+  for (size_t i = 0; i != cAmount; i++)
+  {
+    vector.PushBack(i);
+  }
+
+  for (auto _ : state)
+  {
+    for (auto value : vector)
+    {
+      benchmark::DoNotOptimize(value);
+    }
+  }
+
+  benchmark::DoNotOptimize(vector);
+}
+
+BENCHMARK(FastVector_Iterate);
+
+static void FastVector_NonTrivial_Iterate(benchmark::State& state)
+{
+  constexpr size_t cAmount = 10;
+
+  FastVector<std::string> vector;
+
+  for (size_t i = 0; i != cAmount; i++)
+  {
+    vector.PushBack(std::to_string(i));
+  }
+
+  for (auto _ : state)
+  {
+    for (auto& value : vector)
+    {
+      benchmark::DoNotOptimize(value);
+    }
+  }
+
+  benchmark::DoNotOptimize(vector);
+}
+
+BENCHMARK(FastVector_NonTrivial_Iterate);
+
+static void FastVector_PushBack(benchmark::State& state)
+{
+  for (auto _ : state)
+  {
+    FastVector<size_t> vector;
+
+    vector.PushBack(10);
+
+    benchmark::DoNotOptimize(vector);
+  }
+}
+
+BENCHMARK(FastVector_PushBack);
+
+static void FastVector_PushBack_10(benchmark::State& state)
+{
+  constexpr size_t cAmount = 10;
+
+  for (auto _ : state)
+  {
+    FastVector<size_t> vector;
+
+    for (size_t i = 0; i != cAmount; i++)
+    {
+      vector.PushBack(i);
+    }
+
+    benchmark::DoNotOptimize(vector);
+  }
+}
+
+BENCHMARK(FastVector_PushBack_10);
+
+static void FastVector_PushBack_100(benchmark::State& state)
+{
+  constexpr size_t cAmount = 100;
+
+  for (auto _ : state)
+  {
+    FastVector<size_t> vector;
+
+    for (size_t i = 0; i != cAmount; i++)
+    {
+      vector.PushBack(i);
+    }
+
+    benchmark::DoNotOptimize(vector);
+  }
+}
+
+BENCHMARK(FastVector_PushBack_100);
+
+static void FastVector_PushBack_NonTrivial_100(benchmark::State& state)
+{
+  constexpr size_t cAmount = 100;
+
+  for (auto _ : state)
+  {
+    FastVector<std::string> vector;
+
+    for (size_t i = 0; i != cAmount; i++)
+    {
+      vector.PushBack("TestingString123123");
+    }
+
+    benchmark::DoNotOptimize(vector);
+  }
+}
+
+BENCHMARK(FastVector_PushBack_NonTrivial_100);
+
+} // namespace genebits::engine
--- engine/test/util/fast_vector_tests.cpp
+++ engine/test/util/fast_vector_tests.cpp
@@ -1,10 +1,11 @@
 #include "engine/util/fast_vector.h"
 
+#include <vector>
+
 #include <gtest/gtest.h>
 
 namespace genebits::engine::tests
 {
-
 TEST(FastVector_Tests, Empty_Trivial_AfterDefaultConstruction_True)
 {
   FastVector<double> vector;
@@ -35,12 +36,31 @@ TEST(FastVector_Tests, PushBack_Trivial_Single_SizeIncrease)
   ASSERT_EQ(vector.Size(), 1);
 }
 
+TEST(FastVector_Tests, PushBack_NonTrivial_Single_SizeIncrease)
+{
+  FastVector<std::string> vector;
+
+  vector.PushBack("0");
+
+  ASSERT_EQ(vector.Size(), 1);
+}
+
 TEST(FastVector_Tests, PushBack_Trivial_Double_SizeIncrease)
 {
   FastVector<double> vector;
 
   vector.PushBack(0);
   vector.PushBack(0);
+
+  ASSERT_EQ(vector.Size(), 2);
+}
+
+TEST(FastVector_Tests, PushBack_NonTrivial_Double_SizeIncrease)
+{
+  FastVector<std::string> vector;
+
+  vector.PushBack("0");
+  vector.PushBack("0");
 
   ASSERT_EQ(vector.Size(), 2);
 }
@@ -59,11 +79,36 @@ TEST(FastVector_Tests, PushBack_Trivial_Many_SizeIncrease)
   ASSERT_EQ(vector.Size(), cAmount);
 }
 
+TEST(FastVector_Tests, PushBack_NonTrivial_Many_SizeIncrease)
+{
+  constexpr size_t cAmount = 100;
+
+  FastVector<std::string> vector;
+
+  for (size_t i = 0; i < cAmount; i++)
+  {
+    vector.PushBack(std::to_string(i));
+  }
+
+  ASSERT_EQ(vector.Size(), cAmount);
+}
+
 TEST(FastVector_Tests, PushBack_Trivial_Single_CorrectValue)
 {
   FastVector<double> vector;
 
   const double value = 10;
+
+  vector.PushBack(value);
+
+  ASSERT_EQ(vector[0], value);
+}
+
+TEST(FastVector_Tests, PushBack_NonTrivial_Single_CorrectValue)
+{
+  FastVector<std::string> vector;
+
+  const std::string value = "10";
 
   vector.PushBack(value);
 
@@ -79,6 +124,17 @@ TEST(FastVector_Tests, PushBack_Trivial_Double_CorrectValues)
 
   ASSERT_EQ(vector[0], 10);
   ASSERT_EQ(vector[1], 11);
+}
+
+TEST(FastVector_Tests, PushBack_NonTrivial_Double_CorrectValues)
+{
+  FastVector<std::string> vector;
+
+  vector.PushBack("10");
+  vector.PushBack("11");
+
+  ASSERT_EQ(vector[0], std::string { "10" });
+  ASSERT_EQ(vector[1], std::string { "11" });
 }
 
 TEST(FastVector_Tests, PushBack_Trivial_Many_CorrectValues)
@@ -98,11 +154,39 @@ TEST(FastVector_Tests, PushBack_Trivial_Many_CorrectValues)
   }
 }
 
+TEST(FastVector_Tests, PushBack_NonTrivial_Many_CorrectValues)
+{
+  constexpr size_t cAmount = 100;
+
+  FastVector<std::string> vector;
+
+  for (size_t i = 0; i < cAmount; i++)
+  {
+    vector.PushBack(std::to_string(i));
+  }
+
+  for (size_t i = 0; i < cAmount; i++)
+  {
+    ASSERT_EQ(vector[i], std::to_string(i));
+  }
+}
+
 TEST(FastVector_Tests, PopBack_Trivial_Single_SizeDecrease)
 {
   FastVector<double> vector;
 
   vector.PushBack(0);
+  vector.PopBack();
+
+  ASSERT_EQ(vector.Size(), 0);
+  ASSERT_TRUE(vector.Empty());
+}
+
+TEST(FastVector_Tests, PopBack_NonTrivial_Single_SizeDecrease)
+{
+  FastVector<std::string> vector;
+
+  vector.PushBack("0");
   vector.PopBack();
 
   ASSERT_EQ(vector.Size(), 0);
@@ -122,12 +206,37 @@ TEST(FastVector_Tests, PopBack_Trivial_Double_SizeDecrease)
   ASSERT_TRUE(vector.Empty());
 }
 
+TEST(FastVector_Tests, PopBack_NonTrivial_Double_SizeDecrease)
+{
+  FastVector<std::string> vector;
+
+  vector.PushBack("0");
+  vector.PushBack("0");
+  vector.PopBack();
+  vector.PopBack();
+
+  ASSERT_EQ(vector.Size(), 0);
+  ASSERT_TRUE(vector.Empty());
+}
+
 TEST(FastVector_Tests, PopBack_Trivial_DoublePushSinglePop_SizeDecrease)
 {
   FastVector<double> vector;
 
   vector.PushBack(0);
   vector.PushBack(0);
+  vector.PopBack();
+
+  ASSERT_EQ(vector.Size(), 1);
+  ASSERT_FALSE(vector.Empty());
+}
+
+TEST(FastVector_Tests, PopBack_NonTrivial_DoublePushSinglePop_SizeDecrease)
+{
+  FastVector<std::string> vector;
+
+  vector.PushBack("0");
+  vector.PushBack("0");
   vector.PopBack();
 
   ASSERT_EQ(vector.Size(), 1);
@@ -145,6 +254,67 @@ TEST(FastVector_Tests, PopBack_Trivial_DoublePushSinglePop_CorrectValue)
   ASSERT_EQ(vector[0], 1);
 }
 
+TEST(FastVector_Tests, PopBack_NonTrivial_DoublePushSinglePop_CorrectValue)
+{
+  FastVector<std::string> vector;
+
+  vector.PushBack("1");
+  vector.PushBack("2");
+  vector.PopBack();
+
+  ASSERT_EQ(vector[0], std::string { "1" });
+}
+
+TEST(FastVector_Tests, PopBack_Trivial_PushPopPush_CorrectValues)
+{
+  FastVector<double> vector;
+
+  vector.PushBack(1);
+  vector.PopBack();
+  vector.PushBack(2);
+
+  ASSERT_EQ(vector.Size(), 1);
+  ASSERT_EQ(vector[0], 2);
+}
+
+TEST(FastVector_Tests, PopBack_NonTrivial_PushPopPush_CorrectSize)
+{
+  FastVector<std::string> vector;
+
+  vector.PushBack("1");
+  vector.PopBack();
+  vector.PushBack("2");
+
+  ASSERT_EQ(vector.Size(), 1);
+  ASSERT_EQ(vector[0], std::string { "2" });
+}
+
+TEST(FastVector_Tests, PopBack_Trivial_PushPushPopPush_CorrectValues)
+{
+  FastVector<double> vector;
+
+  vector.PushBack(1);
+  vector.PushBack(2);
+  vector.PopBack();
+  vector.PushBack(3);
+
+  ASSERT_EQ(vector.Size(), 2);
+  ASSERT_EQ(vector[1], 3);
+}
+
+TEST(FastVector_Tests, PopBack_NonTrivial_PushPushPopPush_CorrectValues)
+{
+  FastVector<std::string> vector;
+
+  vector.PushBack("1");
+  vector.PushBack("2");
+  vector.PopBack();
+  vector.PushBack("3");
+
+  ASSERT_EQ(vector.Size(), 2);
+  ASSERT_EQ(vector[1], std::string { "3" });
+}
+
 TEST(FastVector_Tests, BuiltInForEach_Trivial_IterateMany_CorrectValues)
 {
   constexpr size_t cAmount = 100;
@@ -154,6 +324,27 @@ TEST(FastVector_Tests, BuiltInForEach_Trivial_IterateMany_CorrectValues)
   for (size_t i = 0; i < cAmount; i++)
   {
     vector.PushBack(static_cast<double>(i));
+  }
+
+  size_t counter = 0;
+
+  for (auto value : vector)
+  {
+    ASSERT_EQ(vector[counter], value);
+
+    counter++;
+  }
+}
+
+TEST(FastVector_Tests, BuiltInForEach_NonTrivial_IterateMany_CorrectValues)
+{
+  constexpr size_t cAmount = 100;
+
+  FastVector<std::string> vector;
+
+  for (size_t i = 0; i < cAmount; i++)
+  {
+    vector.PushBack(std::to_string(i));
   }
 
   size_t counter = 0;
@@ -257,17 +448,4 @@ TEST(FastVector_Tests, Back_Trivial_CorrectValue)
   ASSERT_EQ(vector.back(), cAmount - 1);
 }
 
-TEST(FastVector_Tests, ConstBack_Trivial_CorrectValue)
-{
-  constexpr size_t cAmount = 20;
-
-  FastVector<double> vector;
-
-  for (size_t i = 0; i < cAmount; i++)
-  {
-    vector.PushBack(static_cast<double>(i));
-  }
-
-  ASSERT_EQ(vector.back(), cAmount - 1);
-}
 } // namespace genebits::engine::tests
