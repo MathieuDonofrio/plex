0a8c812c6883fbdb89fb07a78ff441e76c90db83
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-08-12 22:27:19 -0400

    Testing window events

--- sandbox/hello_world/main.cpp
+++ sandbox/hello_world/main.cpp
@@ -1,4 +1,5 @@
 
+#include <engine/events/listener.h>
 #include <engine/graphics/window.h>
 #include <iostream>
 
@@ -9,22 +10,49 @@ int main(int, char**)
   constexpr WindowCreationHints hints = WindowCreationHints::Defaults;
   auto* window = new Window("Hello world", 256, 256, hints);
 
-  window->Create();
+  struct TestWindowListener : public Listener<TestWindowListener, WindowCloseEvent, WindowFocusEvent, WindowMaximiseEvent, WindowIconifyEvent, WindowResizeEvent, WindowRestoreEvent>
+  {
+    TestWindowListener() : Listener<TestWindowListener, WindowCloseEvent, WindowFocusEvent, WindowMaximiseEvent, WindowIconifyEvent, WindowResizeEvent, WindowRestoreEvent>() {};
 
-  window->SetWindowClosingCallback([](Window* window_ptr)
+    void listen(const WindowCloseEvent&)
     {
-      window_ptr->Close();
-      std::cout << "closing" << std::endl;
-    });
+      std::cout << "window close event" << std::endl;
+    }
+
+    void listen(const WindowFocusEvent& event)
+    {
+      std::cout << "window focus event: " << ((event.state == WindowFocusEventState::Gained) ? "Gained" : "Lost") << std::endl;
+    }
+
+    void listen(const WindowMaximiseEvent&)
+    {
+      std::cout << "window maximise event" << std::endl;
+    }
+
+    void listen(const WindowIconifyEvent&)
+    {
+      std::cout << "window iconify event" << std::endl;
+    }
+
+    void listen(const WindowResizeEvent& event)
+    {
+      std::cout << "window resize event: " << event.width << ", " << event.height << std::endl;
+    }
+
+    void listen(const WindowRestoreEvent& event)
+    {
+      std::cout << &event; // Prevent unused parameter warning treated as an error
+      std::cout << "window restore event" << std::endl;
+    }
+  } TestWindowListener {};
 
   unsigned long long i = 0;
-  while (i < 300)
+  while (i < 600 && !window->IsClosing())
   {
     window->WaitEvents(0.5);
+    window->PollEvents();
     i++;
   }
-
-  window->Close();
 
   return 0;
 }
