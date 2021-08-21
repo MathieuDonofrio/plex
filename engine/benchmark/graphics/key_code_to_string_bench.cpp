c2977c73da531e1e38623015d1b1371e1451d717
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-08-21 13:41:11 -0400

    Added keycode to string and benchmarks

--- engine/benchmark/graphics/key_code_to_string_bench.cpp
+++ engine/benchmark/graphics/key_code_to_string_bench.cpp
@@ -7,29 +7,132 @@
 namespace genebits::engine
 {
 
-static void Random_KeyCodeToString(benchmark::State& state)
+static void Worst_KeyCodeToStringSwitch(benchmark::State& state)
 {
-  Random random(42); // Use runtime seed.
 
+  const uint32_t worst_case = 348;
   for (auto _ : state)
   {
-    int32_t rnd = static_cast<int32_t>(random.NextUInt(349 + 2)) - 2;
-    benchmark::DoNotOptimize(KeyCodeToString(static_cast<KeyCode>(rnd)));
+    benchmark::DoNotOptimize(KeyCodeToStringSwitch(static_cast<KeyCode>(worst_case)));
   }
 }
 
-BENCHMARK(Random_KeyCodeToString);
+BENCHMARK(Worst_KeyCodeToStringSwitch);
+
+const int32_t rnd[] { 238,
+  26,
+  277,
+  141,
+  131,
+  269,
+  338,
+  319,
+  246,
+  90,
+  92,
+  87,
+  6,
+  276,
+  105,
+  326,
+  184,
+  335,
+  323,
+  206,
+  319,
+  185,
+  176,
+  340,
+  68,
+  64,
+  18,
+  21,
+  182,
+  260,
+  261,
+  142,
+  85,
+  164,
+  96,
+  36,
+  320,
+  8,
+  311,
+  42,
+  90,
+  184,
+  258,
+  282,
+  39,
+  174,
+  268,
+  178,
+  49,
+  126,
+  44,
+  344,
+  8,
+  96,
+  57,
+  219,
+  312,
+  124,
+  101,
+  341,
+  125,
+  196,
+  35,
+  75,
+  254,
+  18,
+  85,
+  58,
+  207,
+  136,
+  44,
+  177,
+  102,
+  49,
+  20,
+  54,
+  337,
+  306,
+  34,
+  186,
+  57,
+  89,
+  208,
+  128,
+  48,
+  4,
+  271,
+  166,
+  221,
+  -1,
+  151,
+  152,
+  87,
+  166,
+  345,
+  224,
+  277,
+  340,
+  274,
+  91 };
 
 static void Random_KeyCodeToStringSwitch(benchmark::State& state)
 {
-  Random random(42); // Use runtime seed.
+
+  static uint32_t i = 0;
+  i++;
 
   for (auto _ : state)
   {
-    int32_t rnd = static_cast<int32_t>(random.NextUInt(349 + 2)) - 2;
-    benchmark::DoNotOptimize(KeyCodeToStringSwitch(static_cast<KeyCode>(rnd)));
+    benchmark::DoNotOptimize(KeyCodeToStringSwitch(static_cast<KeyCode>(rnd[i])));
   }
+  if (i == 99) { i = 0; }
 }
 
 BENCHMARK(Random_KeyCodeToStringSwitch);
+
 } // namespace genebits::engine
--- engine/include/genebits/engine/graphics/key_codes.h
+++ engine/include/genebits/engine/graphics/key_codes.h
@@ -1,8 +1,7 @@
 #ifndef GENEBITS_ENGINE_GRAPHICS_KEY_CODES_H
 #define GENEBITS_ENGINE_GRAPHICS_KEY_CODES_H
 
-#include <string_view>
-
+#include <cstdint>
 namespace genebits::engine
 {
 
@@ -132,9 +131,134 @@ enum KeyCode : int32_t
   Menu = 348
 };
 
-const char* KeyCodeToString(KeyCode keycode) noexcept;
-
-const char* KeyCodeToStringSwitch(KeyCode keycode) noexcept;
+constexpr const char* KeyCodeToStringSwitch(KeyCode keycode) noexcept
+{
+  switch (keycode)
+  {
+  case -1: return "Unknown";
+  case 32: return "Space";
+  case 39: return "Apostrophe";
+  case 44: return "Comma";
+  case 45: return "Minus";
+  case 46: return "Period";
+  case 47: return "Slash";
+  case 48: return "Num0";
+  case 49: return "Num1";
+  case 50: return "Num2";
+  case 51: return "Num3";
+  case 52: return "Num4";
+  case 53: return "Num5";
+  case 54: return "Num6";
+  case 55: return "Num7";
+  case 56: return "Num8";
+  case 57: return "Num9";
+  case 59: return "Semicolon";
+  case 61: return "Equal";
+  case 65: return "A";
+  case 66: return "B";
+  case 67: return "C";
+  case 68: return "D";
+  case 69: return "E";
+  case 70: return "F";
+  case 71: return "G";
+  case 72: return "H";
+  case 73: return "I";
+  case 74: return "J";
+  case 75: return "K";
+  case 76: return "L";
+  case 77: return "M";
+  case 78: return "N";
+  case 79: return "O";
+  case 80: return "P";
+  case 81: return "Q";
+  case 82: return "R";
+  case 83: return "S";
+  case 84: return "T";
+  case 85: return "U";
+  case 86: return "V";
+  case 87: return "W";
+  case 88: return "X";
+  case 89: return "Y";
+  case 90: return "Z";
+  case 91: return "LeftBracket";
+  case 92: return "Backslash";
+  case 93: return "RightBracket";
+  case 96: return "GraveAccent";
+  case 161: return "World1";
+  case 162: return "World2";
+  case 256: return "Escape";
+  case 257: return "Enter";
+  case 258: return "Tab";
+  case 259: return "Backspace";
+  case 260: return "Insert";
+  case 261: return "Delete";
+  case 262: return "Right";
+  case 263: return "Left";
+  case 264: return "Down";
+  case 265: return "Up";
+  case 266: return "PageUp";
+  case 267: return "PageDown";
+  case 268: return "Home";
+  case 269: return "End";
+  case 280: return "CapsLock";
+  case 281: return "ScrollLock";
+  case 282: return "NumLock";
+  case 283: return "PrintScreen";
+  case 284: return "Pause";
+  case 290: return "F1";
+  case 291: return "F2";
+  case 292: return "F3";
+  case 293: return "F4";
+  case 294: return "F5";
+  case 295: return "F6";
+  case 296: return "F7";
+  case 297: return "F8";
+  case 298: return "F9";
+  case 299: return "F10";
+  case 300: return "F11";
+  case 301: return "F12";
+  case 302: return "F13";
+  case 303: return "F14";
+  case 304: return "F15";
+  case 305: return "F16";
+  case 306: return "F17";
+  case 307: return "F18";
+  case 308: return "F19";
+  case 309: return "F20";
+  case 310: return "F21";
+  case 311: return "F22";
+  case 312: return "F23";
+  case 313: return "F24";
+  case 314: return "F25";
+  case 320: return "Keypad0";
+  case 321: return "Keypad1";
+  case 322: return "Keypad2";
+  case 323: return "Keypad3";
+  case 324: return "Keypad4";
+  case 325: return "Keypad5";
+  case 326: return "Keypad6";
+  case 327: return "Keypad7";
+  case 328: return "Keypad8";
+  case 329: return "Keypad9";
+  case 330: return "KeypadDecimal";
+  case 331: return "KeypadDivide";
+  case 332: return "KeypadMultiply";
+  case 333: return "KeypadSubtract";
+  case 334: return "KeypadAdd";
+  case 335: return "KeypadEnter";
+  case 336: return "KeypadEqual";
+  case 340: return "LeftShift";
+  case 341: return "LeftControl";
+  case 342: return "LeftAlt";
+  case 343: return "LeftSuper";
+  case 344: return "RightShift";
+  case 345: return "RightControl";
+  case 346: return "RightAlt";
+  case 347: return "RightSuper";
+  case 348: return "Menu";
+  default: return "Unknown";
+  };
+}
 } // namespace genebits::engine
 
 #endif // GENEBITS_KEY_CODES_H
