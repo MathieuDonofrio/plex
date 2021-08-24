af7d296da29a9bd055446defbdc0f9c26ae544ef
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-08-23 23:17:32 -0400

    Modified random benchmark to use compile time RNG list function

--- engine/benchmark/graphics/key_code_to_string_bench.cpp
+++ engine/benchmark/graphics/key_code_to_string_bench.cpp
@@ -1,5 +1,7 @@
 #include "genebits/engine/graphics/key_codes.h"
 
+#include <array>
+
 #include <benchmark/benchmark.h>
 
 #include "genebits/engine/util/random.h"
@@ -19,109 +21,21 @@ static void Worst_KeyCodeToStringSwitch(benchmark::State& state)
 
 BENCHMARK(Worst_KeyCodeToStringSwitch);
 
-const int32_t rnd[] { 238,
-  26,
-  277,
-  141,
-  131,
-  269,
-  338,
-  319,
-  246,
-  90,
-  92,
-  87,
-  6,
-  276,
-  105,
-  326,
-  184,
-  335,
-  323,
-  206,
-  319,
-  185,
-  176,
-  340,
-  68,
-  64,
-  18,
-  21,
-  182,
-  260,
-  261,
-  142,
-  85,
-  164,
-  96,
-  36,
-  320,
-  8,
-  311,
-  42,
-  90,
-  184,
-  258,
-  282,
-  39,
-  174,
-  268,
-  178,
-  49,
-  126,
-  44,
-  344,
-  8,
-  96,
-  57,
-  219,
-  312,
-  124,
-  101,
-  341,
-  125,
-  196,
-  35,
-  75,
-  254,
-  18,
-  85,
-  58,
-  207,
-  136,
-  44,
-  177,
-  102,
-  49,
-  20,
-  54,
-  337,
-  306,
-  34,
-  186,
-  57,
-  89,
-  208,
-  128,
-  48,
-  4,
-  271,
-  166,
-  221,
-  -1,
-  151,
-  152,
-  87,
-  166,
-  345,
-  224,
-  277,
-  340,
-  274,
-  91 };
+template<uint32_t Count>
+consteval auto GenerateRandomKeyCodes()
+{
+  Random rng = Random(CompileTimeSeed());
+  std::array<int32_t, Count> rnd;
+  for (uint32_t i = 0; i < Count; ++i)
+  {
+    rnd[i] = static_cast<int32_t>(rng.NextUInt(0, 348 + 2)) - 2;
+  }
+  return rnd;
+}
 
 static void Random_KeyCodeToStringSwitch(benchmark::State& state)
 {
+  constexpr auto rnd = GenerateRandomKeyCodes<100>();
 
   static uint32_t i = 0;
   i++;
