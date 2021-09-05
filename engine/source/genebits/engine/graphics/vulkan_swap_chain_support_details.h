850021b4f6901dbe9d6450c241929d7f879df40c
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-05 00:15:06 -0400

    Added SwapChainSupportDetails struct

---
+++ engine/source/genebits/engine/graphics/vulkan_swap_chain_support_details.h
@@ -0,0 +1,20 @@
+#ifndef GENEBITS_ENGINE_GRAPHICS_VULKAN_SWAP_CHAIN_SUPPORT_DETAILS_H
+#define GENEBITS_ENGINE_GRAPHICS_VULKAN_SWAP_CHAIN_SUPPORT_DETAILS_H
+
+#include <vector>
+
+#include "vulkan/vulkan_core.h"
+
+namespace genebits::engine
+{
+
+struct VulkanSwapChainSupportDetails
+{
+  VkSurfaceCapabilitiesKHR capabilities;
+  std::vector<VkSurfaceFormatKHR> formats;
+  std::vector<VkPresentModeKHR> present_modes;
+}
+
+} // namespace genebits::engine
+
+#endif
