b095b264229a2e91c7e9e5c7bc53ee428ef0b45c
0xD34DC0DE <emerickpoulin31415@gmail.com>
2022-01-16 22:05:56 -0500

    implement vulkan image

--- engine/source/genebits/engine/graphics/vulkan/vulkan_image.h
+++ engine/source/genebits/engine/graphics/vulkan/vulkan_image.h
@@ -3,6 +3,7 @@
 
 #include "genebits/engine/debug/logging.h"
 #include "genebits/engine/graphics/vulkan/vulkan_device.h"
+#include "genebits/engine/graphics/vulkan/vulkan_render_pass.h"
 
 #include <memory>
 #include <utility>
@@ -16,8 +17,32 @@ class VulkanImage
 {
 public:
   VulkanImage() = default;
+
   virtual ~VulkanImage() = default;
+
+  // This destructor should be in a subclass
+  //  {
+  //    if (image_view_handle_)
+  //    {
+  //      vkDestroyImageView(device_->GetHandle(), image_view_handle_, nullptr);
+  //      image_view_handle_ = VK_NULL_HANDLE;
+  //    }
+  //
+  //    if (image_handle_)
+  //    {
+  //      vkDestroyImage(device_->GetHandle(), image_handle_, nullptr);
+  //      image_handle_ = VK_NULL_HANDLE;
+  //    }
+  //  }
+
 protected:
+  VkImage image_handle_;
+  VkImageView image_view_handle_;
+  std::shared_ptr<VulkanDevice> device_;
+
+  VulkanImage(VkImage image_handle, std::shared_ptr<VulkanDevice> device)
+    : image_handle_(image_handle), device_(std::move(device)), image_view_handle_(VK_NULL_HANDLE)
+  {}
 };
 
 } // namespace genebits::engine
