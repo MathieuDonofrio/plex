ca9382099f36721854a069cb69db60d2d9039097
0xD34DC0DE <emerickpoulin31415@gmail.com>
2022-02-13 23:10:39 -0500

    Use ColorAttachment

--- engine/source/genebits/engine/graphics/vulkan/vulkan_present_attachment.h
+++ engine/source/genebits/engine/graphics/vulkan/vulkan_present_attachment.h
@@ -1,22 +1,20 @@
 #ifndef GENEBITS_ENGINE_GRAPHICS_VULKAN_PRESENT_ATTACHMENT_H
 #define GENEBITS_ENGINE_GRAPHICS_VULKAN_PRESENT_ATTACHMENT_H
 
-#include "genebits/engine/graphics/vulkan/vulkan_attachment.h"
+#include "genebits/engine/graphics/vulkan/vulkan_color_attachment.h"
 
 namespace genebits::engine
 {
 
-class VulkanPresentAttachment : public VulkanAttachment
+class VulkanPresentAttachment : public VulkanColorAttachment
 {
 public:
-  VulkanPresentAttachment(
-    uint32_t attachment_index, VkFormat format)
-    : VulkanAttachment(attachment_index,
+  VulkanPresentAttachment(uint32_t attachment_index, VkFormat format)
+    : VulkanColorAttachment(attachment_index,
       format,
       VK_ATTACHMENT_LOAD_OP_CLEAR, // Clear buffer when starting render pass
       VK_ATTACHMENT_STORE_OP_STORE, // Keep results after render pass
       VK_IMAGE_LAYOUT_UNDEFINED,
-      VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
       VK_IMAGE_LAYOUT_PRESENT_SRC_KHR)
   {}
 
