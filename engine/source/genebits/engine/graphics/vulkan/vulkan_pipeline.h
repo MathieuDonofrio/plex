d217cef5d2a70049fce1fef9936a5d1bc6e68ce0
0xD34DC0DE <emerickpoulin31415@gmail.com>
2022-03-13 17:49:28 -0400

    start implementing pipeline

---
+++ engine/source/genebits/engine/graphics/vulkan/vulkan_pipeline.h
@@ -0,0 +1,13 @@
+#ifndef GENEBITS_ENGINE_GRAPHICS_VULKAN_PIPELINE_H
+#define GENEBITS_ENGINE_GRAPHICS_VULKAN_PIPELINE_H
+
+namespace genebits::engine {
+
+class VulkanPipeline {
+public:
+private:
+};
+
+}
+
+#endif // GENEBITS_ENGINE_GRAPHICS_VULKAN_PIPELINE_H
