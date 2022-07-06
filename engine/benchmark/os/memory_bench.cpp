765f1591fe134cc1c879b0c9ec504ed6138a7bba
LAPTOP-EK3CB67C\Mathieu <ptimatt@live.ca>
2022-07-05 20:32:12 -0400

    added benchmark to compare relocation to memcpy and memmove

--- engine/benchmark/os/memory_bench.cpp
+++ engine/benchmark/os/memory_bench.cpp
@@ -17,7 +17,8 @@ static void Memory_STD_Memcpy(benchmark::State& state)
 
   for (auto _ : state)
   {
-    std::memcpy(dst, src, bytes);
+    benchmark::DoNotOptimize(std::memcpy(dst, src, bytes));
+    benchmark::ClobberMemory();
   }
 
   benchmark::DoNotOptimize(src);
@@ -25,6 +26,8 @@ static void Memory_STD_Memcpy(benchmark::State& state)
 
   ::operator delete(src, bytes, std::align_val_t(32));
   ::operator delete(dst, bytes, std::align_val_t(32));
+
+  state.SetBytesProcessed(bytes);
 }
 
 BENCHMARK(Memory_STD_Memcpy)->Arg(100)->Arg(1000)->Arg(10000);
@@ -40,7 +43,8 @@ static void Memory_STD_Memmove(benchmark::State& state)
 
   for (auto _ : state)
   {
-    std::memmove(dst, src, bytes);
+    benchmark::DoNotOptimize(std::memmove(dst, src, bytes));
+    benchmark::ClobberMemory();
   }
 
   benchmark::DoNotOptimize(src);
@@ -48,8 +52,40 @@ static void Memory_STD_Memmove(benchmark::State& state)
 
   ::operator delete(src, bytes, std::align_val_t(32));
   ::operator delete(dst, bytes, std::align_val_t(32));
+
+  state.SetBytesProcessed(bytes);
 }
 
 BENCHMARK(Memory_STD_Memmove)->Arg(100)->Arg(1000)->Arg(10000);
+
+static void Memory_STD_UninitializedRelocate(benchmark::State& state)
+{
+  const size_t amount = state.range(0);
+
+  const size_t bytes = 32 * amount;
+
+  void* src = ::operator new(bytes, std::align_val_t(32));
+  void* dst = ::operator new(bytes, std::align_val_t(32));
+
+  char* first = reinterpret_cast<char*>(src);
+  char* last = first + bytes;
+  char* result = reinterpret_cast<char*>(dst);
+
+  for (auto _ : state)
+  {
+    benchmark::DoNotOptimize(UninitializedRelocate(first, last, result));
+    benchmark::ClobberMemory();
+  }
+
+  benchmark::DoNotOptimize(src);
+  benchmark::DoNotOptimize(dst);
+
+  ::operator delete(src, bytes, std::align_val_t(32));
+  ::operator delete(dst, bytes, std::align_val_t(32));
+
+  state.SetBytesProcessed(bytes);
+}
+
+BENCHMARK(Memory_STD_UninitializedRelocate)->Arg(100)->Arg(1000)->Arg(10000);
 
 } // namespace genebits::engine::bench
