3ebddc96d8ce28ddbede258c25d78b78be6e071b
0xD34DC0DE <emerickpoulin31415@gmail.com>
2022-09-17 12:16:43 -0400

    Update cmake-conan to support msvc 19.33

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -4,10 +4,10 @@
 
 if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
   message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-  file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake"
-       "${CMAKE_BINARY_DIR}/conan.cmake"
-       EXPECTED_HASH SHA256=396e16d0f5eabdc6a14afddbcfff62a54a7ee75c6da23f32f7a31bc85db23484
-       TLS_VERIFY ON)
+  file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+          "${CMAKE_BINARY_DIR}/conan.cmake"
+          EXPECTED_HASH SHA256=5cdb3042632da3efff558924eecefd580a0e786863a857ca097c3d1d43df5dcd
+          TLS_VERIFY ON)
 endif ()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
